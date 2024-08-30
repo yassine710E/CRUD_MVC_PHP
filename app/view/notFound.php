69bf6ee1cc60688884c6099b08016ab897bed79e
yassine chabane <yassinechabane71@gmail.com>
2024-08-30 16:44:53 +0100

    modify some html and fill README.md file

--- app/view/notFound.php
+++ app/view/notFound.php
@@ -6,7 +6,7 @@ $title="error 404";
 require "layout/header.php";
 ?>
 
-<h1 style="color:red"><i>Error 400</i></h1> 
+<h1 style="color:red">Error 400</h1> 
 
 <?php
 //require the layout/footer page ti avoid DRY
